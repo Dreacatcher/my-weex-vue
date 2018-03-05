997ad32d1d25967e5dcc74631794a385cd959ee4
lucm <lucm@dcholdings.com>
2018-03-05 10:29:50 +0800

    feature:dd

---
+++ my-weex-vue-demo/src/components/ui/weexMyTabBarParent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="weexMyTabBarParentWp">
+    <slot>
+    </slot>
+  </div>
+</template>
+<script>
+// import common from 'common'
+export default {
+  name: 'weexMyTabBarParent',
+  props: {
+    upCallBack: {
+      type: Function
+    },
+    downCallBack: {
+      type: Function
+    },
+    upLoadingTip: {
+      type: String
+    },
+    downLoadingTip: {
+      type: String
+    }
+  },
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style type="text/scss" lang="scss">
+@import '../../../src/style/_include/_all';
+</style>
