c5d8d7d3e37440f1af3e6724d85f6fe076d343d5
Dreacatcher <1363341151@qq.com>
2018-03-14 09:58:29 +0800

    'update'

--- my-weex-vue-demo/.temp/router/index.js
+++ my-weex-vue-demo/.temp/router/index.js
@@ -1,25 +1,22 @@
 import Vue from 'vue'
-
 import MyVue from 'vue'
 import Router from 'vue-router'
 import HomeVue from 'demoVue'
 import demorRouter from './_include/demo'
 /* eslint-disable*/
 if (WXEnvironment.platform == 'Web') {
-  window.Vue = MyVue
+    window.Vue = MyVue
 }
 Vue.use(Router)
-let newArr = [
-  {
+let newArr = [{
     path: '/',
     name: 'HomeVue',
     component: HomeVue
-  }
-]
+}]
 let newRouter = [
-  ...newArr,
-  ...demorRouter
+    ...newArr,
+    ...demorRouter
 ]
 module.exports = new Router({
-  routes: newRouter
+    routes: newRouter
 })
--- my-weex-vue-demo/src/router/index.js
+++ my-weex-vue-demo/src/router/index.js
@@ -1,24 +1,21 @@
-
 import MyVue from 'vue'
 import Router from 'vue-router'
 import HomeVue from 'demoVue'
 import demorRouter from './_include/demo'
 /* eslint-disable*/
 if (WXEnvironment.platform == 'Web') {
-  window.Vue = MyVue
+    window.Vue = MyVue
 }
 Vue.use(Router)
-let newArr = [
-  {
+let newArr = [{
     path: '/',
     name: 'HomeVue',
     component: HomeVue
-  }
-]
+}]
 let newRouter = [
-  ...newArr,
-  ...demorRouter
+    ...newArr,
+    ...demorRouter
 ]
 module.exports = new Router({
-  routes: newRouter
+    routes: newRouter
 })
