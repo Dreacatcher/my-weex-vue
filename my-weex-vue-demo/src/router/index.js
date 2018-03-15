aa60b8bc380a24e4b62ce32564bff5334f34ad1d
Dreacatcher <1363341151@qq.com>
2018-03-15 11:15:54 +0800

    'featutre'

--- my-weex-vue-demo/src/router/index.js
+++ my-weex-vue-demo/src/router/index.js
@@ -24,7 +24,7 @@ let newRouter = [
     ...dataServicesRouter,
     ...myProjectsRouter,
     ...usersCenterRouter,
-    ...workOrdersRouter,
+    ...workOrdersRouter
 ]
 module.exports = new Router({
     routes: newRouter
