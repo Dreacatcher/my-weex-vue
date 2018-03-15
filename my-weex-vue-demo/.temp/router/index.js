f17d970b6f7d7d2320794ddb08f029c512cfa779
Dreacatcher <1363341151@qq.com>
2018-03-15 14:42:37 +0800

    'feature'

--- my-weex-vue-demo/.temp/router/index.js
+++ my-weex-vue-demo/.temp/router/index.js
@@ -3,6 +3,11 @@ import MyVue from 'vue'
 import Router from 'vue-router'
 import HomeVue from 'demoVue'
 import demorRouter from './_include/demo'
+import messegeRouter from './_include/messege'
+import dataServicesRouter from './_include/dataServices'
+import myProjectsRouter from './_include/myProjects'
+import usersCenterRouter from './_include/usersCenter'
+import workOrdersRouter from './_include/workOrders'
 /* eslint-disable*/
 if (WXEnvironment.platform == 'Web') {
     window.Vue = MyVue
@@ -15,7 +20,12 @@ let newArr = [{
 }]
 let newRouter = [
     ...newArr,
-    ...demorRouter
+    ...demorRouter,
+    ...messegeRouter,
+    ...dataServicesRouter,
+    ...myProjectsRouter,
+    ...usersCenterRouter,
+    ...workOrdersRouter
 ]
 module.exports = new Router({
     routes: newRouter
