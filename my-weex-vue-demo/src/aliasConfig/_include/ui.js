84cd50079086f97eca92ec705a1fbcf4fe7556be
Dreacatcher <1363341151@qq.com>
2018-03-16 17:13:05 +0800

    'feature:ddd'

--- my-weex-vue-demo/src/aliasConfig/_include/ui.js
+++ my-weex-vue-demo/src/aliasConfig/_include/ui.js
@@ -2,17 +2,17 @@
  * @Author: lcm
  * @Date:   2017-04-17 18:01:21
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-03-16 17:10:18
+ * @Last Modified time: 2018-03-16 17:12:45
  */
 
 'use strict'
 var path = require('path')
 var srcDir = path.resolve(process.cwd(), 'src')
 module.exports = {
-    // ******************************************demo ******************************//
-    actionSheet: srcDir + '/components/ui/actionSheet',
-    weexMyTabBar: srcDir + '/components/ui/weexMyTabBar',
-    weexMyTabBarParent: srcDir + '/components/ui/weexMyTabBarParent',
-    imgBtn: srcDir + '/components/ui/imgBtn',
-    mySlider: srcDir + '/components/ui/mySlider'
+  // ******************************************demo ******************************//
+  actionSheet: srcDir + '/components/ui/actionSheet',
+  weexMyTabBar: srcDir + '/components/ui/weexMyTabBar',
+  weexMyTabBarParent: srcDir + '/components/ui/weexMyTabBarParent',
+  imgBtn: srcDir + '/components/ui/imgBtn',
+  mySlider: srcDir + '/components/ui/mySlider'
 }
