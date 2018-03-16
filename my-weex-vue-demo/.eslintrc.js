9e4c0b5b542edc7977d42a1921206f90b3a3e719
Dreacatcher <1363341151@qq.com>
2018-03-16 09:18:49 +0800

    'feature:增加eslint规范'

--- my-weex-vue-demo/.eslintrc.js
+++ my-weex-vue-demo/.eslintrc.js
@@ -28,10 +28,12 @@ module.exports = {
         'generator-star-spacing': 'off',
         "space-before-function-paren": [0, "always"],
         // 强制使用一致的反勾号、单引号
-        "quotes": [2, "single"],
+        "quotes": [2, "single"], // 单引号
         "indent": ["error", 2], //强制一致的缩进风格
         "no-unused-vars": [2, { "vars": "all", "args": "after-used" }], //不允许有声明后未使用的变量或者参数
         "no-use-before-define": [2, "nofunc"], //不允许在未定义之前就使用变量
+        "eol-last": ["error", "never"], // 最后一行不启用换行
+        "lines-around-comment": [1, { "beforeBlockComment": false, "beforeLineComment": false }],
         // allow debugger during development
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
     }
--- my-weex-vue-demo/src/router/_include/myProjects.js
+++ my-weex-vue-demo/src/router/_include/myProjects.js
@@ -1,7 +1,7 @@
 import ActionSheet from 'actionSheet'
 export default [{
-    path: '/actionSheet',
-    name: 'ActionSheet',
-    component: ActionSheet
-        // meta: { keepAlive: true } // 缓存组件（true：缓存，false: 不缓存，默认为false）
+  path: '/actionSheet',
+  name: 'ActionSheet',
+  component: ActionSheet
+  // meta: { keepAlive: true } 缓存组件（true：缓存，false: 不缓存，默认为false）
 }]
